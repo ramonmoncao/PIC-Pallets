404e713c515ec097e875a553fa5ee7d7448f7aa8
Ramon Monção <ramonmoncao@hotmail.com>
2024-11-05 19:46:57 -0300

    build configs

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
+import cors from 'cors';
 import express from 'express'; // Importa o Express
 import swaggerUi from 'swagger-ui-express';
 import routes from './routes'; // Importa as rotas
 import swaggerFile from './swagger.json';
 
 const app = express() // Cria uma instância do Express
-const PORT = 3000 // Define a porta do servidor
+const PORT = process.env.PORT || 3000 // Define a porta do servidor
+app.use(cors)
 app.use(express.json()) // Habilita o uso de JSON nas requisições
 
 app.use('/api', routes) // Define o prefixo para as rotas
