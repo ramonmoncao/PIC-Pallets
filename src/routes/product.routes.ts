1b58f2cc1b31d2073262954c5ac35c1e6c76b876
Erik Gomes <gomeserik35@gmail.com>
2024-11-26 17:33:19 -0300

    add routes

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createProduct, deleteProduct, findAllProducts, updateProduct } from "../controllers/product.controller";
+import { createProduct, deleteProduct, findAllProducts, findProductById, updateProduct } from "../controllers/product.controller";
 import { CreateProductDto, UpdateProductDto } from "../dtos/product.dto";
 import { validate } from "../middlewares/validate.middleware";
 
@@ -7,6 +7,7 @@ const router = Router()
 
 router.post('/', validate(CreateProductDto), createProduct)
 router.get('/', findAllProducts)
+router.get('/:id', findProductById)
 router.patch('/:id', validate(UpdateProductDto), updateProduct)
 router.delete('/:id', deleteProduct)
 
